72a8cf3246458e66f298e6aed0d03eed0949dded
DmitryDobro <dobronovich_95@mail.ru>
2022-09-07 23:12:25 +0300

    page

---
+++ assets/829f9795cd084940f449.js
@@ -0,0 +1,1 @@
+import"./styles/scss/main.scss";import"./scripts/acardeon";import"./scripts/cityBox";import"./scripts/slider";import"./scripts/mobile";import"./scripts/grafic";import"./scripts/sortCard";import"./scripts/modal";
